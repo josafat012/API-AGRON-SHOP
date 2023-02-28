7dc5106b5950ab48b2ba847bf5ff9be6124d609e
josafat012 <107165470+josafat012@users.noreply.github.com>
2023-02-28 10:54:05 -0600

    [ADD] update

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -22,9 +22,9 @@ const validLogin = async (req, res) => {
       return res.status(404).send({ message: "Usuario no encontrado" })
     } 
     if (username === user.nombreUsuario) {
-      console.log("paso if user")
+      // console.log("paso if user")
       if(password === user.contrasena){
-        console.log('paso if passw')
+        // console.log('paso if passw')
         // datos.push(user._id, user.esVendedor, user.nombreUsuario)
         // console.log(username, password, datos)
         return res.status(200).send({ message: "Has iniciado sesion 😀" })
